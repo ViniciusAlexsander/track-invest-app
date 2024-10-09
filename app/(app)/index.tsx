5e58d701570297af58e31343825c5653b68dca62
Vinicius Marinho <viniciusalexsander1@gmail.com>
2024-10-09 00:06:52 -0300

    feat: iniciando provider de autenticação

---
+++ app/(app)/index.tsx
@@ -0,0 +1,19 @@
+import { useSession } from "@/components/AuthContext";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  const { signOut } = useSession();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>TA LOGADO</Text>
+      <Text
+        onPress={() => {
+          // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
+          signOut();
+        }}
+      >
+        Sign Out
+      </Text>
+    </View>
+  );
+}
